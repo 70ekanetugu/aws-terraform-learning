5d36d90ba5a59f7910a2829c827ef7ddad1b6d11
70ekanetugu <12kanetugu@gmail.com>
2025-01-26 16:01:59 +0000

    update tflint setting

--- .tflint.hcl
+++ .tflint.hcl
@@ -4,3 +4,15 @@ plugin "aws" {
     version = "0.37.0"
     source  = "github.com/terraform-linters/tflint-ruleset-aws"
 }
+
+rule terraform_required_version {
+    enabled = false
+}
+
+rule terraform_required_providers {
+    enabled = false
+}
+
+rule terraform_deprecated_index {
+    enabled = false
+}
--- check.sh
+++ check.sh
@@ -1,11 +1,21 @@
 #!/bin/sh
 
-echo "Start: terraform format"
+echo "[Start]: terraform format"
 terraform fmt --recursive
-echo "Complete!"
+echo "\e[1;34mCompleted\e[0m format\n"
 
-echo "Start: terraform validate"
+echo "[Start]: terraform validate"
 terraform validate
 
-echo "Start: tflint..."
-tflint
+if [ $? -ne 0 ]; then
+    echo "Validation failed"
+    exit 1
+fi
+
+echo "[Start]: tflint"
+tflint --config "$(pwd)/.tflint.hcl" --recursive
+
+if [ $? -eq 0 ]; then
+    echo "\e[1;32mSuccess!\e[0m Style is valid.\n"
+    echo "All check done!"
+fi
