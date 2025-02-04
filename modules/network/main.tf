38e6a12ace2ffe0a78cd23a6c19b0f82fa96d373
70ekanetugu <12kanetugu@gmail.com>
2025-02-04 14:45:54 +0000

    add network module

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -20,14 +20,20 @@ resource "aws_vpc" "demo" {
   }
 }
 
+# =================================================================================
+# IGW
+#
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.demo.id
 
   tags = {
-    Name = var.env
+    Name = "${var.env}-igw"
   }
 }
 
+# =================================================================================
+# サブネット
+#
 resource "aws_subnet" "public" {
   count = var.subnet_count
 
@@ -37,7 +43,7 @@ resource "aws_subnet" "public" {
   map_public_ip_on_launch = true
 
   tags = {
-    Name = "${var.env}-${local.availability_zones[count.index]}"
+    Name = "${var.env}-public-${local.availability_zones[count.index]}"
   }
 }
 
@@ -49,6 +55,84 @@ resource "aws_subnet" "private" {
   availability_zone = "ap-northeast-${local.availability_zones[count.index]}"
 
   tags = {
-    Name = "${var.env}-${local.availability_zones[count.index]}"
+    Name = "${var.env}-private-${local.availability_zones[count.index]}"
   }
+}
+
+# =================================================================================
+# NAT
+#
+resource "aws_eip" "ngw" {
+  count = var.subnet_count
+
+  domain = "vpc"
+
+  tags = {
+    Name = "demo-ngw"
+  }
+
+  # NATは暗黙的にIGWに依存しているため明示が必要。
+  depends_on = [aws_internet_gateway.igw]
+}
+resource "aws_nat_gateway" "ngw" {
+  count = var.subnet_count
+
+  allocation_id = aws_eip.ngw[count.index].id
+  subnet_id     = aws_subnet.public[0].id
+
+  tags = {
+    Name = "demo-ngw${count.index}"
+  }
+
+  # NATは暗黙的にIGWに依存しているため明示が必要。
+  depends_on = [aws_internet_gateway.igw]
+}
+
+# =================================================================================
+# ルーティング
+#
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.demo.id
+
+  tags = {
+    Name = "${var.env}-public-rtb"
+  }
+}
+
+resource "aws_route" "public" {
+  route_table_id         = aws_route_table.public.id
+  gateway_id             = aws_internet_gateway.igw.id
+  destination_cidr_block = "0.0.0.0/0"
+}
+
+resource "aws_route_table_association" "public" {
+  count = var.subnet_count
+
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table" "private" {
+  count = var.subnet_count
+
+  vpc_id = aws_vpc.demo.id
+
+  tags = {
+    Name = "${var.env}-private-rtb${count.index}"
+  }
+}
+
+resource "aws_route" "private" {
+  count = var.subnet_count
+
+  route_table_id         = aws_route_table.private[count.index].id
+  nat_gateway_id         = aws_nat_gateway.ngw[count.index].id
+  destination_cidr_block = "0.0.0.0/0"
+}
+
+resource "aws_route_table_association" "private" {
+  count = var.subnet_count
+
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private[count.index].id
 }
