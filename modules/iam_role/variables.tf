ce670dae2cfabf3b27f6a6b3a71fee0376016a65
70ekanetugu <12kanetugu@gmail.com>
2025-02-11 02:46:05 +0000

    add iam role

---
+++ modules/iam_role/variables.tf
@@ -0,0 +1,14 @@
+variable "name" {
+  description = "IAM role name"
+  type        = string
+}
+
+variable "policy" {
+  description = "IAM policy"
+  type        = string
+}
+
+variable "identifier" {
+  description = "Principals identifer"
+  type        = string
+}
