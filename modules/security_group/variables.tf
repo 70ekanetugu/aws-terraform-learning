608b6afcb0aa688438d3ea9ecad4663f868321c1
70ekanetugu <12kanetugu@gmail.com>
2025-02-11 17:09:28 +0000

    add sg

---
+++ modules/security_group/variables.tf
@@ -0,0 +1,27 @@
+variable "ingress_port_and_protocol" {
+  description = "Ingress port and protocol pairs. Port that allow TCP, UDP port number. Protocol is 'tcp', 'udp', '-1'."
+  type        = list(map(string))
+  # default = [{
+  #   port = "-1" 
+  #   protocol = "-1"
+  # }]
+}
+
+variable "ingress_source" {
+  description = "Map have keys 'cidr_ipv4' and 'referenced_sg_id'. Key 'cidr_ipv4' ignore if referenced_security_group_id is set."
+  type        = map(string)
+  # default = {
+  #   cidr_ipv4 = "0.0.0.0/0"
+  #   referenced_sg_id = ""
+  # }
+}
+
+variable "name" {
+  description = "For security group. This name also use tags 'Name'."
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "VPC id"
+  type        = string
+}
