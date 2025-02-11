c776ae25b44f32a1c5e01f16f0a536beb4e60fe8
70ekanetugu <12kanetugu@gmail.com>
2025-02-11 03:31:54 +0000

    add front ap

--- front.tf
+++ front.tf
@@ -6,7 +6,7 @@ resource "aws_lb" "front" {
   internal                   = false
   idle_timeout               = 60
   enable_deletion_protection = false
-
+  security_groups            = [module.public_alb_sg.security_group_id]
   subnets = [
     aws_subnet.public_1a.id,
     aws_subnet.public_1c.id
@@ -16,10 +16,6 @@ resource "aws_lb" "front" {
     bucket  = aws_s3_bucket.alb_log.id
     enabled = true
   }
-
-  security_groups = [
-    module.public_alb_sg.security_group_id
-  ]
 
   tags = {
     Name = "demo-front-alb"
@@ -51,14 +47,14 @@ resource "aws_lb_target_group" "front" {
   vpc_id               = aws_vpc.demo.id
   port                 = 80
   protocol             = "HTTP"
-  deregistration_delay = 15 # ターゲットの登録解除前にALBが待機する時間(s)。デフォルトは300
+  deregistration_delay = 300 # ターゲットの登録解除前にALBが待機する時間(s)。デフォルトは300
 
   health_check {
     path                = "/"
-    healthy_threshold   = 3
+    healthy_threshold   = 5
     unhealthy_threshold = 2
     timeout             = 5
-    interval            = 10
+    interval            = 30
     matcher             = 200
     port                = "traffic-port"
     protocol            = "HTTP"
@@ -106,6 +102,14 @@ resource "aws_ecs_task_definition" "front" {
       "name": "demo-front",
       "image": "nginx:latest",
       "essential": true,
+      "logConfiguration": {
+        "logDriver": "awslogs",
+        "options": {
+          "awslogs-region": "ap-northeast-1",
+          "awslogs-stream-prefix": "nginx",
+          "awslogs-group": "/demo/front"
+        }
+      },
       "portMappings": [
         {
           "protocol": "tcp",
@@ -115,6 +119,7 @@ resource "aws_ecs_task_definition" "front" {
     }
   ]
   JSON
+  execution_role_arn       = module.ecs_task_execution_role.iam_role_arn
 }
 resource "aws_ecs_service" "front" {
   name                              = "demo-front-service"
@@ -123,7 +128,7 @@ resource "aws_ecs_service" "front" {
   desired_count                     = 2
   launch_type                       = "FARGATE"
   platform_version                  = "1.4.0"
-  health_check_grace_period_seconds = 30
+  health_check_grace_period_seconds = 60
 
   network_configuration {
     assign_public_ip = false
@@ -142,6 +147,35 @@ resource "aws_ecs_service" "front" {
   }
 
   lifecycle {
-    ignore_changes = [task_definition]
+    # ignore_changes = [task_definition]
   }
+}
+
+#
+# CloudWatch Logs
+#
+resource "aws_cloudwatch_log_group" "front" {
+  name              = "/demo/front"
+  retention_in_days = 180
+}
+
+data "aws_iam_policy" "front" {
+  arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+data "aws_iam_policy_document" "front" {
+  source_policy_documents = [data.aws_iam_policy.front.policy]
+
+  statement {
+    effect    = "Allow"
+    actions   = ["ssm:GetParameters", "kms:Decrypt"]
+    resources = ["*"]
+  }
+}
+
+module "ecs_task_execution_role" {
+  source     = "./modules/iam_role"
+  name       = "ecs-task-execution"
+  identifier = "ecs-tasks.amazonaws.com"
+  policy     = data.aws_iam_policy_document.front.json
 }
